2336684dbc99aec3fa1b1317a4120e875cf7c084
Kirill <kirill0392@gmail.com>
2024-04-02 20:25:57 +0300

    css

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-<html lang="en">
+let timerId; // переменная, которая будет хранить ID таймера
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  НУЖНО ПОКЛЮЧИТЬ ЭТОТ ШРИФТ: https://fonts.googleapis.com/css2?family=Russo+One&display=swap
-  <link rel="stylesheet" href="styles.css">
-  <title>homeWork_4</title>
-</head>
+const startButton = ДОКУМЕНТ.ГЕТэЛЕМЕНТбАЙаЙДИ(КАКОЙ - ТО);
+startButton.ЭДэВЕНТлИСТНЕР("ТЯП", function () {
+	// НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ setInterval
+	timerId = СЕТиНТРЕВАЛ(updateClock, ОДНАсЕКУНДА); // запускаем  updateClock() каждую секунду
+});
 
-<body>
-<div class="clockCase">
-  <div>
-    <h1 id="clock">00:00:00</h1>
-    <div class="buttonCase">
-      <button id="start">Запустить часы</button>
-      <button id="stop">Остановить часы</button>
-    </div>
-  </div>
-</div>
+const stopButton = ДОКУМЕНТ.ГЕТэЛЕМЕНТбАЙаЙДИ(КАКОЙ - ТО);
+stopButton.addEventListener("ТЯП", function () {
+	// НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ clearInterval
+	clearInterval(timerId); // останавливаем таймер
+});
 
-<script src="index.js"></script>
-</body>
-
-</html>
+function updateClock() {
+	const clock = document.getElementById("clock");
+	// НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ new Date()
+	const now = НОВАЯдАТА();
+	const hours = now.ДАЙчАСЫ();
+	const minutes = now.ДАЙмИНУТЫ();
+	const seconds = now.ДАЙсЕКУНДЫ();
+	clock.textContent = hours + ":" + minutes + ":" + seconds;
+}
