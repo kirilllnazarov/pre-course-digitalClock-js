4432cb5cb6544ac5a526ca28bfad75fdf3202e9e
Kirill <kirill0392@gmail.com>
2024-04-03 00:16:07 +0300

    1

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-let timerId; // переменная, которая будет хранить ID таймера
-
 const startButton = document.getElementById("start");
 startButton.addEventListener("click", function () {
-	// НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ setInterval
-	timerId = setInterval(updateClock, 1000); // запускаем  updateClock() каждую секунду
+	timerId = setInterval(updateClock, 1000);
 });
 
 const stopButton = document.getElementById("stop");
 stopButton.addEventListener("click", function () {
-	// НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ clearInterval
-	clearInterval(timerId); // останавливаем таймер
+	clearInterval(timerId);
 });
 
 function updateClock() {
 	const clock = document.getElementById("clock");
-	// НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ new Date()
 	const now = new Date();
 	const hours = now.getHours();
 	const minutes = now.getMinutes();
